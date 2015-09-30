93bde587ea74e536553a9226fbafdcd295d8fcdc
Juhan Ernits <juhan.ernits@ttu.ee>
2015-09-30 12:03:39 +0300

    Added today's lecture and next coursework.

---
+++ coursework3.fsx
@@ -0,0 +1,73 @@
+(*
+
+  ITT8060 -- Advanced Programming 2015
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 3: User defined types
+
+  ------------------------------------
+  Name:
+  TUT Student ID:
+  ------------------------------------
+
+
+  Answer the questions below.  You answers to questions 1--5 should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. It has to be submitted to the TUT
+  git system using the instructions on the course web page by October 9, 2015.
+*)
+
+// 1. Make a function rearrange: Fexpr -> Fexpr that will rearrange a finite expression tree (as defined in
+// the lecture in the differentiation example) in such a way that constants always are to the left of variables
+// in addition and multiplication and the variables with higher power in multiplication are pushed to the right.
+
+// 2. Make a function simplify: Fexpr -> Fexpr that will simplify a finite expression tree by removing
+// terms that evaluate to zero.
+
+// Given the type definition:
+// type BList =
+//  | BEmpty
+//  | Snoc of BList * int
+// 
+// 3. Make the function filterB: (prop: int -> bool) BList -> BList that will return a list for the elements of which
+// the function prop returns true.
+
+// 4. Make the function mapB: (trans: int -> int) BList -> BList that will return a list where the function trans has
+// been applied to each element.
+
+// Given the type definition
+// type Tree =
+//  | Nil
+//  | Branch2 of Tree * int * Tree
+//  | Branch3 of Tree * int * Tree * int * Tree
+// 
+// 5. Define the value exampleTree : Tree that represents the following
+//    tree:
+//
+//        2
+//       / \
+//      *  3 5
+//        / | \
+//       *  *  *
+
+// 6. Define a function sumTree : Tree -> int that computes the sum of
+//    all labels in the given tree.
+
+// 7. Define a function productTree : Tree -> int that computes the
+//    product of all labels in the given tree. If this function
+//    encounters a label 0, it shall not look at any further labels, but
+//    return 0 right away.
+
+// ** Bonus questions **
+
+// 8. Define a function mapTree : (int -> int) -> Tree -> Tree that
+//    applies the given function to every label of the given tree.
+
+// 9. Use mapTree to implement a function negateAll : Tree -> Tree that
+//    negates all labels of a given tree.
---
+++ lecture4.fsx
@@ -0,0 +1,165 @@
+﻿(*
+
+  ITT8060 -- Advanced Programming 2015
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+  Lecture 4: Multiple arguments and multiple results,
+             user defined data types,
+             recursive data types,
+             option
+
+  Juhan Ernits
+
+*)
+
+
+let pair =  11, "Hello!"
+
+let divRem n m = n / m, n % m
+
+let divRem' (n,  m) = n / m, n % m
+
+divRem' (7, 2)
+
+open System
+
+Int32.TryParse "11"
+
+Int32.TryParse "F#"
+
+let triple = 11, "Hello!", 'A'
+let leftLeaning = (11, "Hello!"), 'A'
+let rightLeaning = 11, ("Hello!", 'A')
+
+let printPlaceInfo name (latitude, longitude) = 
+    printfn "%s @ (%f deg, %f deg)" name latitude longitude
+    
+let tut = "Tallinn University of Technology", (59.3950, 24.6719)
+
+let tutName, tutPos = tut
+printPlaceInfo tutName tutPos
+
+// user defined types
+
+type Lat = float
+type Lon = float
+
+type Fexpr = | Const of float
+             | X
+             | Add of Fexpr * Fexpr
+             | Sub of Fexpr * Fexpr
+             | Mul of Fexpr * Fexpr
+             | Div of Fexpr * Fexpr
+             | Sin of Fexpr
+             | Cos of Fexpr
+             | Log of Fexpr
+             | Exp of Fexpr
+
+let rec D fe =
+   match fe with
+   | Const _       -> Const 0.0
+   | X             -> Const 1.0
+   | Add(fe1,fe2)  -> Add(D fe1, D fe2)
+   | Sub(fe1,fe2)  -> Sub(D fe1, D fe2)
+   | Mul(fe1,fe2)  -> Add(Mul(D fe1, fe2), Mul(fe1,D fe2))
+   | Div(fe1,fe2)  -> Div(Sub(Mul(D fe1, fe2), Mul(fe1,D fe2)), Mul(fe2,fe2))
+   | Sin fe1       -> Mul(Cos fe1, D fe1)
+   | Cos fe1       -> Mul(Const -1.0, Mul(Sin fe1, D fe1))
+   | Log fe1       -> Div(D fe1, fe1)
+   | Exp fe1       -> Mul(Exp fe1, D fe1)
+
+let example1 = Mul(Const 2.0, Mul(X,Mul(X,X)))
+let example2 = Exp(X)
+
+D example1
+D example2
+
+
+type IntList =
+  | Empty
+  | NonEmpty of int * IntList
+
+let rec sumIntList list = 
+  match list with
+  | Empty   -> 0 
+  | NonEmpty (head, tail) -> head + sumIntList tail
+
+let list1 = NonEmpty(1, NonEmpty(2, NonEmpty(3,Empty)))
+
+sumIntList list1
+
+let rec mapIntList (trans : int -> int) list =
+  match list with
+  | Empty                 -> Empty
+  | NonEmpty (head, tail) -> NonEmpty (trans head, mapIntList trans tail)
+
+let addOne = (+) 1
+
+mapIntList addOne list1
+
+mapIntList ((+) 2) list1
+
+let rec filterIntList (prop: int -> bool) list = 
+  match list with
+   | Empty                 -> Empty
+   | NonEmpty (head, tail) -> let filteredTail = filterIntList prop tail
+                              if prop head
+                              then NonEmpty (head, filteredTail)
+                              else filteredTail
+
+let equalsToTwo = (=) 2
+
+filterIntList equalsToTwo list1
+filterIntList ((<) 1) list1
+
+
+type Tree =
+  | Leaf of int
+  | Node of Tree * Tree
+
+let tree1 = Leaf 2
+let tree2 = Node (Leaf 3, Leaf 5)
+let tree3 = Node (tree1, tree2)
+
+let rec sumTree tree = 
+  match tree with
+  | Leaf label          -> label
+  | Node (left, right)  -> sumTree left + sumTree right
+
+
+sumTree tree1
+sumTree tree2
+sumTree tree3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
