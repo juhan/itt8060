7fbc9fa03ab6e0d033d1f1233e48239c586874f7
Juhan Ernits <juhan.ernits@ttu.ee>
2015-11-25 12:24:52 +0200

    Lecture notes from today and a new coursework.

---
+++ coursework8.fsx
@@ -0,0 +1,46 @@
+(*
+
+  ITT8060 -- Advanced Programming 2015
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------------------
+
+  Coursework 8: Sequences and computation expressions
+
+  ------------------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------------------
+
+
+  Answer the questions below. You answers to the questions should be correct F#
+  code written after the question. This file is an F# script file; it should be
+  possible to load the whole file at once. If you can't, then you have
+  introduced a syntax error somewhere.
+
+  This coursework will be graded.
+
+  Commit and push your solution to the repository as file coursework8.fsx in
+  directory coursework8.
+
+  The deadline for completing the above procedure is Friday, December 4, 2015.
+
+  We will consider the submission to be the latest version of the appropriate
+  files in the appropriate directory before the deadline of a particular
+  coursework.
+
+*)
+
+// 1. Make a declaration for the sequence of odd numbers.
+
+// 2. Declare a function that, for given i and n, selects the sublist [a_i;a_i+1;...;a_i+n-1]
+//    of sequence [a_0; a_1; ...]
+
+// 3. Use the functions in the Seq library to declare a function cartesian sqx sqy that gives
+// a sequence containing all pairs (x,y) where x is a member of sqx and y is a member of sqy.
+
+// 4. Make an alternative solution to 3 using sequence expressions.
+
+// 5. Extend the logging workflow explained in the lecture to support downloading web pages.
+// Log the http response code for each URL. Make a function that takes a sequence of URLs and 
+// downloads them while logging the response codes using the logging workflow.
---
+++ lecture12.fsx
@@ -0,0 +1,191 @@
+﻿(*
+
+   Lecture Nov 25, 2015: Sequences and computation expressions
+
+   Based on Chapter 12 of RWFP (available for free online:
+   https://manning-content.s3.amazonaws.com/download/0/9e61459-5d6a-43a3-a407-d73a9112ba77/SampleChapter12.pdf)
+
+   Additional reading: Chapter 11 and 12 from Functional programming using F#
+
+*)
+
+let x = seq [1..10]
+let y = seq [|1..10|]
+
+Seq.find(fun x' -> x' = 9) y
+Seq.toList y
+
+let num = seq {
+   let n = 10
+   yield n + 1
+   printfn "second .."
+   yield n + 2
+}
+
+Seq.take 1 num
+Seq.take 2 num
+Seq.toList num
+num
+
+let capitals = ["Tallinn" ; "Prague"]
+
+let withNew x =
+   seq { yield x
+         yield "New " + x
+       }
+
+let allCities =
+   seq {
+       yield "Seattle"
+       yield! capitals
+       yield! withNew "York"
+   }
+
+Seq.take 2 allCities
+List.ofSeq allCities 
+allCities |> List.ofSeq
+
+open System
+
+let rec factorialsUtil num factorial = seq {
+   if (factorial < 10000000) then
+       yield String.Format ("{0}! = {1}", num, factorial)
+       yield sprintf "%d! = %d" num factorial
+   //if (factorial < 100) then 
+       let num = num + 1
+       yield! factorialsUtil num (factorial * num)
+
+}
+
+let factorials =
+    factorialsUtil 0 1
+
+factorials |> List.ofSeq
+
+Seq.take 2 factorials
+
+open System
+open System.Drawing
+
+let rnd = new Random()
+#nowarn "40"
+let rec colorsRnd =
+    seq { let c() = rnd.Next(256)
+          yield Color.FromArgb (c(), c(), c())
+          yield! colorsRnd
+         }
+
+Seq.take 3 colorsRnd
+
+let cities = [("New York", "USA"); ("London", "UK")
+              ("Cambridge", "UK"); ("Cambridge", "USA")]
+
+let entered = ["London"; "Cambridge"]
+
+seq {
+   for name in entered do
+      for (n,c) in cities do
+         if (n = name) then
+             yield sprintf "%s from %s" n c
+}
+
+entered |> Seq.collect (fun name -> 
+      seq {
+          for (n,c) in cities do
+              if (n = name) then
+                 yield sprintf "%s from %s" n c
+      }
+)
+
+entered |> Seq.collect (fun name -> 
+    cities |> Seq.collect (fun (n,c) ->
+        if (n <> name) then []
+        else [sprintf "%s from %s" n c]))
+
+
+open System
+
+type ValueWrapper<'a> =
+    | Value of 'a
+
+type ValueWrapperBuilder() =
+    member x.Bind (Value (v), f)  = f v
+    member x.Return(v) = Value v
+
+let value = new ValueWrapperBuilder()
+
+let readInt() = value {
+   let n = Int32.Parse(Console.ReadLine())
+   return n
+}
+
+readInt()
+
+let v = 
+   value {
+      let! n = readInt()
+      let! m = readInt()
+      let add = n + m
+      let sub = n - m
+      return add * sub
+   }
+
+module Logging =
+    open System
+
+    type LoggingValue<'a> =
+          | Log of 'a * list<string>
+
+    type LoggingBuilder() =
+      member x.Bind(Log(v,logs1), f) =
+           let (Log(nv,logs2)) = f (v)
+           Log (nv, logs1 @ logs2)
+
+      member x.Return(v) = Log(v, [])
+      member x.Zero() = Log((),[])
+
+    let log = new LoggingBuilder()
+    let logMessage(s) = Log((), [s])
+    let write(s) = log {
+        do! logMessage("Writing: " + s)
+        Console.Write(s) }
+
+    let read() = log {
+        do! logMessage("Reading ...")
+        return Console.ReadLine() }
+
+    let testIt  ()= log {
+       do! logMessage("Starting ...")
+       do! write("Enter your name:")
+       let! name = read()
+       return "Hello "+ name + "!"
+    }
+
+
+
+Logging.testIt ()
+
+// The compiler translates the computation expression syntax to the following:
+module Translations =
+    
+    let write(s) =
+      log.Bind(logMessage("writing: " + s), fun () ->
+         Console.Write(s)
+         log.Zero())
+
+    let read() = 
+      log.Bind(logMessage("reading"), fun () ->
+        log.Return(Console.ReadLine()))
+
+    let testIt() = 
+      log.Bind(logMessage("starting"), fun () ->
+        log.Bind(write("Enter name: "), fun () ->
+          log.Bind(read(), fun name ->
+            log.Return("Hello " + name + "!"))))
+
+
+
+
+
+
+
